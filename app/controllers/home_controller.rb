2dcc5f07ae1110ce2a1a804b6752388bb9f14303
Jessica Owens <jessicadanaowens@gmail.com>
2015-08-05 21:22:21 -0600

    TextAnalyzer output is rendered in the index.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
 
+    text = "I love skiing."
+
+    text_analyzer = TextAnalyzer.new(text).show_sentiment
+
+    @sentiment = text_analyzer
+
   end
 end
